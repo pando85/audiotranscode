8de1dd9cf95748a7a41738fe765888e423d5f30b
Pando85 <pando855@gmail.com>
2016-01-12 13:36:56 +0100

    Reestructure data.

--- setup.py
+++ setup.py
@@ -1,12 +1,39 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name='Audiotranscode',
-      version='0.1',
-      description='python module to transcode between audio formats using the CLI frontends of the codecs already installed on your system, e.g lame, vorbis-tools etc',
-      author='Pando85',
-      author_email='pando855@gmail.com',
-      url='https://github.com/pando85/audiotranscode',
-      packages=['audiotranscode'],
-     )
+import os
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='Audiotranscode',
+    version='0.1',
+    description='Python module to transcode between audio formats',
+    long_description=read('README'),
+    author='Pando85',
+    author_email='pando855@gmail.com',
+    url='https://github.com/pando85/audiotranscode',
+    license='GPLv3',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Multimedia',
+        'Topic :: Multimedia :: Sound/Audio',
+    ],
+    tests_require=["nose"],
+)
