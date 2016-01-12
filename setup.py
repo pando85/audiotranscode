f666c7d59ba6ba909d27d7dd68e8ee291868a5e0
Pando85 <pando855@gmail.com>
2016-01-12 13:18:11 +0100

    Added setup.py to be callable from pip

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Audiotranscode',
+      version='0.1',
+      description='python module to transcode between audio formats using the CLI frontends of the codecs already installed on your system, e.g lame, vorbis-tools etc',
+      author='Pando85',
+      author_email='pando855@gmail.com',
+      url='https://github.com/pando85/audiotranscode',
+      packages=['audiotranscode'],
+     )
